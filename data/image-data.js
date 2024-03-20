2591a53fe7ba53d5e9513ec461281268a76f1a78
Yuya Kawai <yuyakawai.github@gmail.com>
2024-03-20 15:52:22 +0900

    Import images from image-data.js

---
+++ data/image-data.js
@@ -0,0 +1,44 @@
+export const images = [
+  {
+    name: "bird_wing_up",
+    path: "image/bird_wing_up.png",
+    element: null,
+    isLoaded: false,
+  },
+  {
+    name: "bird_wing_down",
+    path: "image/bird_wing_down.png",
+    element: null,
+    isLoaded: false,
+  },
+  {
+    name: "bird_damage",
+    path: "image/bird_damage.png",
+    element: null,
+    isLoaded: false,
+  },
+  {
+    name: "heart_full",
+    path: "image/heart_full.png",
+    element: null,
+    isLoaded: false,
+  },
+  {
+    name: "heart_empty",
+    path: "image/heart_empty.png",
+    element: null,
+    isLoaded: false,
+  },
+  {
+    name: "balloon",
+    path: "image/balloon.png",
+    element: null,
+    isLoaded: false,
+  },
+  {
+    name: "scarecrow",
+    path: "image/scarecrow.png",
+    element: null,
+    isLoaded: false,
+  },
+];
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import { images } from "./data/image-data.js";
 import { enemyData } from "./data/enemy-data.js";
 import { mapData } from "./data/map-data.js";
 
@@ -37,51 +38,6 @@ const canvas = {
 };
 
 const loaderContainer = { progressBarElement: null, messageElement: null };
-
-const images = [
-  {
-    name: "bird_wing_up",
-    path: "image/bird_wing_up.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "bird_wing_down",
-    path: "image/bird_wing_down.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "bird_damage",
-    path: "image/bird_damage.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "heart_full",
-    path: "image/heart_full.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "heart_empty",
-    path: "image/heart_empty.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "balloon",
-    path: "image/balloon.png",
-    element: null,
-    isLoaded: false,
-  },
-  {
-    name: "scarecrow",
-    path: "image/scarecrow.png",
-    element: null,
-    isLoaded: false,
-  },
-];
 
 const init = () => {
   mainContainer.element = document.getElementById("main-container");
