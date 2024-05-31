1e8c7cc513c64ade9d3aa73c3ac6534b28aebd1a
codecommit-test-user <register@yuyakawai.com>
2024-05-31 19:51:06 +0900

    Update enemy movement speed

--- data/enemy-data.js
+++ data/enemy-data.js
@@ -8,7 +8,8 @@ export const enemyData = [
     update: (e) => {
       if (e.option.isMove) {
         e.option.time++;
-        if (e.option.time > 1000) {
+        console.log(e.option.time);
+        if (e.option.time > 600) {
           e.option.initSpeedX += e.option.accelerationX;
           e.option.initSpeedY += e.option.accelerationY;
           e.x += e.option.initSpeedX;
